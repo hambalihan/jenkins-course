4f6c225b7c8a05553a52bd347e442ca23f4134fd
hambalihan <hambalihani@gmail.com>
2020-03-18 17:17:16 +0700

    changing job name

--- job-dsl/nodejsaws.groovy
+++ job-dsl/nodejsaws.groovy
@@ -1,4 +1,4 @@
-job('NodeJS Docker example') {
+job('NodeJS on AWS ECR') {
     scm {
         git('git://github.com/hambalihan/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('hambalihan')
