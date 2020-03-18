247d8a748fb53a6938953f5584bf465551c7886a
Hani Budhiman <42445459+hambalihan@users.noreply.github.com>
2020-03-18 13:50:38 +0700

    changing git repos

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/hambalihan/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('hambalihan')
+            node / gitConfigEmail('hambalihani@gmail.com')
         }
     }
     triggers {
