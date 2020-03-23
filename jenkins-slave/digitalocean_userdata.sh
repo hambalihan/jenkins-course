d2d9c19b0b515824701341a12e218d2445e1ac59
Hani Budhiman <42445459+hambalihan@users.noreply.github.com>
2020-03-23 14:23:41 +0700

    changing docker repository to mine

--- jenkins-slave/digitalocean_userdata.sh
+++ jenkins-slave/digitalocean_userdata.sh
@@ -9,4 +9,4 @@ cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
 chmod 700 /var/jenkins_home/.ssh
 chmod 600 /var/jenkins_home/.ssh/authorized_keys
 chown -R 1000:1000 /var/jenkins_home
-docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d wardviaene/jenkins-slave
+docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d hambali/jenkins-slave
